096da88a45e367a4231db43341ca8afb817f3d2d
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-20 20:10:29 -0800

    Add sql db tasks

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Creates a database for dev and creates user
+-- if they do not exists
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS
+       'hbnb_dev'@'localhost'
+       IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
