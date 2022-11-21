0a599511f9c77ebf7c2193321b14902f84559072
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-20 20:06:33 -0800

    Add sql db tasks

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Creates a database for test and creates user
+-- if they do not exists
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS
+       'hbnb_test'@'localhost'
+       IDENTIFIED BY 'hbnb_test_pwd';
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
