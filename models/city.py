c400023e287d96ebb7705f7720b7d1219d01ce2b
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-21 21:04:04 -0800

    Add: import for Base

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel, Base):
