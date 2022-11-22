2a9e151f35d70e336052bb36287cd6a9a8535bb7
Luke Hackett <luke-hackett@outlook.com>
2022-11-22 17:23:42 +1100

    remove try else condition

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -15,16 +16,12 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            try:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-                del kwargs['__class__']
-            except:
-                pass
-            finally:
-                self.__dict__.update(kwargs)
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
