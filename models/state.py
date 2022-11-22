9a43676b1cbd18e20cc6ffb98c8b4f005bf3251a
Luke Hackett <luke-hackett@outlook.com>
2022-11-23 08:31:05 +1100

    add filestorage method

--- models/state.py
+++ models/state.py
@@ -4,15 +4,37 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-
+from engine.file_storage import FileStorage
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(), nullable=False)
-    cities = relationship("City", backref="state")
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        backref="state",
+        cascade="all, delete")
 
     # I think more needs to be added. Need to add a getter
     # for the FileStorage and I think we may have to have an if/else
     # statement to check if it's in db storage or filestorage
+    @property
+    def cities(self):
+        '''
+        If in filestorage mode this will
+        return a list with all cities that have the state_id as
+        a foreign key reference.
+        '''
+        city_list = []
+        fs = FileStorage()
+        '''
+        Use the method from filestorage to return list of cities.
+        Currently no way toi truncate list, will just be dict that is 
+        filtered through.
+        '''
+        city_dict = fs.all(City.__class__.__name__)
+        for city in city_dict:
+            if city.get('state_id') == self.id:
+                city_list.append(city)
+        return city_list
