292500b53b4e46c4fdff56acc5a21ebfb13b1b52
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-21 21:04:41 -0800

    Started file, needs more work

--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(), nullable=False)
+    cities = relationship("City", backref="state")
+
+    # I think more needs to be added. Need to add a getter
+    # for the FileStorage and I think we may have to have an if/else
+    # statement to check if it's in db storage or filestorage
