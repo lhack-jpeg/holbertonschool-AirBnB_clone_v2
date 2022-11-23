f335ab0d89ad8c8ca90b41208200ee361c4a2ed8
klmana <104742971+klmana@users.noreply.github.com>
2022-11-23 21:05:21 +1100

    task8

--- models/city.py
+++ models/city.py
@@ -5,5 +5,7 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade='all, delete', backref='cities')"
