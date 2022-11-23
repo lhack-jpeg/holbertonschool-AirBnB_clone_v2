b5de09da1079e56e0f89cb1eab8ed629259ddf9a
Luke Hackett <luke-hackett@outlook.com>
2022-11-24 10:14:41 +1100

    Add relationship for reviews

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -12,3 +13,8 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    reviews = relationship(
+        'Review',
+        backref='user',
+        cascade='all, delete'
+    )
