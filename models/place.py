8a8c026a0d8a8aca8618687967a409e633a01eee
Luke Hackett <luke-hackett@outlook.com>
2022-11-23 18:21:07 -0800

    fix typo

--- models/place.py
+++ models/place.py
@@ -12,7 +12,7 @@ place_amenity = Table(
     'place_amenity',
     Base.metadata,
     Column('place_id', String(60),
-           ForeignKey('places.id'), nullable=False),
+           ForeignKey('places.id'), primary_key=True, nullable=False),
     Column('amenity_id', String(60), ForeignKey(
         'amenities.id'), primary_key=True, nullable=False),
 )
