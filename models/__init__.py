227b43df0cf061339f3bf622bb9603937c90ade5
Luke Hackett <luke-hackett@outlook.com>
2022-11-23 20:38:21 -0800

    removed comments

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-'''
+
 from models.review import Review
 from models.amenity import Amenity
 from models.city import City
@@ -10,7 +10,7 @@ from models.state import State
 from models.place import Place
 from models.user import User
 from models.base_model import BaseModel, Base
-'''
+
 from os import getenv
 
 storage_method = getenv('HBNB_TYPE_STORAGE')
