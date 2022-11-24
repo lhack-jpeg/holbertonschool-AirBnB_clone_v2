61a9bb6d06b340c8e350b1dfaac3cf1f192bd388
Luke Hackett <luke-hackett@outlook.com>
2022-11-23 22:19:20 -0800

    remove print debug in to_dict func

--- models/base_model.py
+++ models/base_model.py
@@ -65,7 +65,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        print(dictionary.keys())
         if '_sa_instance_state' in dictionary.keys():
             del dictionary["_sa_instance_state"]
         return dictionary
