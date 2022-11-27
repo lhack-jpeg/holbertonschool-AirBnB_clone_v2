bf14090ea71a44df3d2c65779bd401688b47160c
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-27 15:06:07 -0800

    Fix: pycodestyling

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hell0():
     """returns greeting when route is called"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
