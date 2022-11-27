d01bf08fea1f8508973cf0d00e171c6f3974a83d
root <km@rtin.mobi>
2022-11-26 16:00:17 -0800

    add init to test review file

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -27,3 +27,7 @@ class test_review(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.text), str)
+
+        
+if __name__ == "__main__":
+    unittest.main()
