e0a600864785049bd0ec4515faf9d88ab7fb2742
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-27 20:23:50 -0800

    Test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-Routes:
-    /: display “Hello HBNB!”
-    /hbnb: display “HBNB”
-    /c/<text>: display “C ” followed by the value of the text variable
-        (replace underscore _ symbols with a space )
-"""
+"""starts a flask web app listenning on port 5000"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return ('Hello HBNB!')
+@app.route("/", strict_slashes=False)
+def hello():
+    """returns a greeting when route is queried"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return ('HBNB')
+    """returns hbnb for /hbnb query"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def cPath(text):
-    """returns “C ” followed by the value of the text variable"""
-    return ('C ' + text.replace('_', ' '))
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """displays url text on page"""
+    return "C " + text.replace("_", " ")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
