1e50891d5dcac7d7f8de6763bfb1596ce47fa83e
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-27 20:20:05 -0800

    Change: docs

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-starts a web app using flask, listening on port 5000
-"""
+""" starts a web app using flask, listening on port 5000 """
 
 from flask import Flask
 app = Flask(__name__)
