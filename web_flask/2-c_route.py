8401c63cc173e955bf0936055279cfb4e16527a8
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-27 21:46:50 -0800

    Test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""starts a flask web app listenning on port 5000"""
+""" starts a web app using flask, listening on port 5000 """
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    """returns a greeting when route is queried"""
+def hell0():
+    """returns greeting when route is called"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """returns hbnb for /hbnb query"""
+    """returns hbnb when route hbnb is called"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def display_text(text):
-    """displays url text on page"""
-    return "C " + text.replace("_", " ")
+def c_text(text):
+    """displays url <text> on page"""
+    return f"C " + text.replace("_", " ")
 
 
 if __name__ == "__main__":
