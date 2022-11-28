8b01eb56eb9bce05b5da18639304ff02773b0594
Luke Hackett <luke-hackett@outlook.com>
2022-11-28 02:01:02 -0800

    Fix pycodestyle

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Script for very basic web application. Returns the string
+requested by any device on the network depending on the phrases
+after the slash.
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def school():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is(text):
+    # returns the string 'C <text>'
+    return f'C {text}'.replace('_', " ")
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_is(text):
+    # returns the string 'python is...' defaults to is cool
+    return f'Python {text}'.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ web_flask/3-python_route.py~
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Script for very basic web application. Returns the string
+requested by any device on the network depending on the phrases
+after the slash.
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def school():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is(text):
+    # returns the string 'C <text>'
+    return f'C {text}'.replace('_', " ")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
