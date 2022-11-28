4f5aafce883deb93f3ca82d3978dd490953f260c
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-27 18:50:13 -0800

    Fix: pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,11 @@
   Using flask, listening on port 5000
 """
 
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hell0():
@@ -14,6 +16,7 @@ def hell0():
       Returns text when route is called
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
