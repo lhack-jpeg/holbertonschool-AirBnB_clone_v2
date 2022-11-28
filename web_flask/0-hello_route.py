b40a3fb17e80fd6445b468aae1e6dcdbdd2098bc
lhack-jpeg <luke-hackett@outlook.com>
2022-11-28 20:33:35 +1100

    add check if main file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,8 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def index():
-    return 'Hello HBNB!'
 
-app.run(host='0.0.0.0', port='5000')
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
