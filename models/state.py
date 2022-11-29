86f2afcddac4d68599c2f7876c29875b32639825
Luke Hackett <luke-hackett@outlook.com>
2022-11-29 15:40:17 +1100

    add check for filestorage method

--- models/state.py
+++ models/state.py
@@ -5,36 +5,39 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 import models.engine.file_storage
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City",
-        backref="state",
-        cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete")
 
     # I think more needs to be added. Need to add a getter
     # for the FileStorage and I think we may have to have an if/else
     # statement to check if it's in db storage or filestorage
-    @property
-    def cities(self):
-        '''
-        If in filestorage mode this will
-        return a list with all cities that have the state_id as
-        a foreign key reference.
-        '''
-        city_list = []
-        fs = file_storage.FileStorage()
-        '''
-        Use the method from filestorage to return list of cities.
-        Currently no way toi truncate list, will just be dict that is
-        filtered through.
-        '''
-        city_dict = fs.all(City.__class__.__name__)
-        for city in city_dict:
-            if city.get('state_id') == self.id:
-                city_list.append(city)
-        return city_list
+    else:
+        @property
+        def cities(self):
+            '''
+            If in filestorage mode this will
+            return a list with all cities that have the state_id as
+            a foreign key reference.
+            '''
+            city_list = []
+            fs = file_storage.FileStorage()
+            '''
+            Use the method from filestorage to return list of cities.
+            Currently no way toi truncate list, will just be dict that is
+            filtered through.
+            '''
+            city_dict = fs.all(City.__class__.__name__)
+            for city in city_dict:
+                if city.get('state_id') == self.id:
+                    city_list.append(city)
+            return city_list
