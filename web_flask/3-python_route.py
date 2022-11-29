b393c44903dfb13bbd67aa0b50b284b8b7701d85
Luke Hackett <luke-hackett@outlook.com>
2022-11-29 03:22:47 +0000

    add strict slashes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,13 +21,13 @@ def school():
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_is(text):
     ''' returns the string "C <text>".'''
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>')
 def python_is(text):
     '''Returns the string "python is..." defaults to is cool.'''
