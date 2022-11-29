9abbe4113954dd274ffdbf33f74101ae6e18b125
Luke Hackett <luke-hackett@outlook.com>
2022-11-29 10:04:03 +0000

    Create ordered list of tuples from dict and render to html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,12 +4,13 @@ This script returns the list of states in the database.
 '''
 from flask import Flask, render_template
 from models import storage
+from operator import itemgetter
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db():
+def teardown_db(exception):
     '''Close storage connect when the session is popped.'''
     storage.close()
 
@@ -17,8 +18,17 @@ def teardown_db():
 @app.route('/states_list', strict_slashes=False)
 def all_state_list():
     '''Gets all states in the area.'''
-    state_list = storage.all('State').value()
-    print(state_list)
+    state_list = storage.all('State').values()
+    state_tuple_list = []
+    for state in state_list:
+        state_tuple = (state.id, state.name)
+        state_tuple_list.append(state_tuple)
+    
+    ordered_state_list = sorted(state_tuple_list, key=itemgetter(1))
+    return render_template(
+        '7-states_list.html', 
+        ordered_state_list=ordered_state_list
+    )
 
 
 if __name__ == "__main__":
