0b130ac4c2d57ad95a55087895db3e9a1b1641ad
Luke Hackett <luke-hackett@outlook.com>
2022-11-29 15:40:26 +1100

    add close method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -90,3 +90,7 @@ class DBStorage():
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
         self.__session = Session()
+
+    def close(self):
+        '''Close the currrent session.'''
+        self.__session.remove()
