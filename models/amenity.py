a1a033f0f08b23b5aacac88822736b69f9d19180
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 09:18:18 +1100

    add lazy joined

--- models/amenity.py
+++ models/amenity.py
@@ -14,6 +14,7 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
         place_amenities = relationship('Place',
                                        secondary=place_amenity,
-                                       back_populates='amenities')
+                                       back_populates='amenities',
+                                       lazy='joined')
     else:
         name = ""
